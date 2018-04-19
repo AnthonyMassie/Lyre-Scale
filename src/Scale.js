b3f7907cff4d70c66af8e3f373df75d55e2cc3ad
ganth <ganth@DESKTOP-KR54074.PK5001Z>
2018-04-19 00:38:16 -0400

    More initial files

--- src/Scale.js
+++ src/Scale.js
@@ -1,5 +1,19 @@
 "use strict";
 
-class Scale {}
+var Note = require("@lyre/note");
+
+class Scale {
+    constructor(root) {
+        this.root = root;
+    }
+    
+    getNote(n) {
+        return this.root.getTransposition(this.definition[n-1]);
+    }
+    
+    setDefinition(definition) {
+        this.definition = definition;
+    }
+}
 
 module.exports = Scale;
--- test/Scale.js
+++ test/Scale.js
@@ -1,1 +1,13 @@
+'use strict';
+
+var chai = require("chai");
+var expect  = chai.expect;
 var Scale = require("../index.js");
+
+describe("Scale", function() {
+    describe("does things", function() {
+        it("and stuff", function() {
+            
+        });
+    });
+});
